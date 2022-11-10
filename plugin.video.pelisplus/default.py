1c27172cc5865a34e2a364daedf11526eb2fc9bf
crowslayer <31776660+crowslayer@users.noreply.github.com>
2022-11-10 11:53:31 -0600

    Update default.py
    Update Url domain.

--- plugin.video.pelisplus/default.py
+++ plugin.video.pelisplus/default.py
@@ -12,7 +12,7 @@ from resources.lib.modules import control
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-BASEURL = 'https://pelisplushd.is/'
+BASEURL = 'https://pelisplushd.nz/'
 
 ADDON       = xbmcaddon.Addon()
 ADDON_DATA  = ADDON.getAddonInfo('profile')
